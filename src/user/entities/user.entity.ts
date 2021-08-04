075fcc87dab66e7bdecb17bdc5874e3e3b35ee6d
정훈박 <fireking5997@gmail.com>
2021-08-04 21:43:29 +0900

    휴대폰 번호 11자리로 변경

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -50,7 +50,7 @@ export class User extends CoreEntity {
   @Column({ nullable: true })
   @IsString({ message: '올바른 휴대폰번호 형식이 아닙니다.' })
   @IsOptional()
-  @Matches(/^[0-9]{3}[-]+[0-9]{4}[-]+[0-9]{4}$/i, {
+  @Matches(/^[0-9]{11}$/i, {
     message: '휴대폰번호를 형식에 맞게 입력하세요.',
   })
   phoneNumber?: string;
