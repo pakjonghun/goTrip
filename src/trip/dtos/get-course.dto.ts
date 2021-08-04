3a1e5c926136e8bfbe63d71fad851a931d23c70f
coffeed-cat <gwcattower@gmail.com>
2021-08-04 19:03:05 +0900

    [ get-course.dto.ts, trip.constants.ts ] 존재하지 않는 카테고리, 지역코드가 들어올 때의 예외처리

--- src/trip/dtos/get-course.dto.ts
+++ src/trip/dtos/get-course.dto.ts
@@ -1,16 +1,27 @@
-import { IsArray, IsDateString, IsNumber, IsString } from 'class-validator';
+import {
+  IsArray,
+  IsDateString,
+  IsIn,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 import { CommonOutput } from 'src/common/dtos/common.dto';
 import { Course } from '../entities/course.entity';
+import { AREA_CODES, CATEGORIES } from '../trip.constants';
 
 export class GetCourseInput {
   @IsDateString()
   startDate: Date;
 
   @IsArray()
+  @IsIn(CATEGORIES, { each: true })
   category: string[];
 
   @IsArray()
-  areaCode: number;
+  @IsIn(AREA_CODES, {
+    each: true,
+  })
+  areaCode: number[];
 
   @IsString()
   lat: string;
