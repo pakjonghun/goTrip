46ea9b9b14f49c770661463b9c2f24e095d1a4ed
coffeed-cat <gwcattower@gmail.com>
2021-08-04 16:57:21 +0900

    [ trip.module.ts, trip.service.ts ] TourAPI 코드 분리, 안쓰는 리포지토리 삭제

--- src/trip/trip.module.ts
+++ src/trip/trip.module.ts
@@ -2,17 +2,11 @@ import { Module } from '@nestjs/common';
 import { TripService } from './trip.service';
 import { TripController } from './trip.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Course } from './entities/course.entity';
-import { AreaCode } from './entities/areacode.entity';
 import { Location } from './entities/location.entity';
 import { GeoModule } from 'src/geo/geo.module';
-import { DetailAreaCode } from './entities/detailAreaCode.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Course, AreaCode, Location, DetailAreaCode]),
-    GeoModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Location]), GeoModule],
   providers: [TripService],
   controllers: [TripController],
 })
