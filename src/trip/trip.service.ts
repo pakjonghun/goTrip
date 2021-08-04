68cb3898b89041b3792a0fe1bba71890cc0da22f
coffeed-cat <gwcattower@gmail.com>
2021-08-04 10:11:11 +0900

    DB 데이터 이전 함수 추가

--- src/trip/trip.service.ts
+++ src/trip/trip.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
 import { GeoService } from 'src/geo/geo.service';
 import { Repository } from 'typeorm';
 import { GetCourseProcessOutput } from './dtos/get-course-process.dto';
 import { GetCourseInput, GetCourseOutput } from './dtos/get-course.dto';
+import { AreaCode } from './entities/areacode.entity';
 import { Course } from './entities/course.entity';
 import { Location } from './entities/location.entity';
 
@@ -14,6 +16,8 @@ export class TripService {
     @InjectRepository(Course) private readonly courses: Repository<Course>,
     @InjectRepository(Location)
     private readonly locations: Repository<Location>,
+    @InjectRepository(AreaCode)
+    private readonly areacodes: Repository<AreaCode>,
   ) {}
 
   async getCourse(getCourseInput: GetCourseInput): Promise<GetCourseOutput> {
@@ -39,47 +43,58 @@ export class TripService {
     return { ok: true, courseProcess };
   }
 
-  // async moveData() {
-  //   await axios.get('http://localhost:4000/detail/move').then(async (res) => {
-  //     const data = res.data;
+  async moveAreaCodes() {
+    await axios.get('http://localhost:4000/detail/move').then(async (res) => {
+      const data = res.data;
+      for (const i of data) {
+        await this.areacodes.save(
+          this.areacodes.create({ code: i.code, name: i.name }),
+        );
+      }
+    });
+  }
 
-  //     for (const obj of data) {
-  //       for (const loketsi of obj.course) {
-  //         console.log(loketsi);
-  //         const newLoketsi = this.locations.create({
-  //           contentid: loketsi.contentid,
-  //           subcontentid: loketsi.subcontentid,
-  //           contenttypeid: loketsi.contenttypeid,
-  //           name: loketsi.subname,
-  //           overview: loketsi.subdetailoverview,
-  //           img: loketsi.subdetailimg,
-  //         });
+  async moveData() {
+    await axios.get('http://localhost:4000/detail/move').then(async (res) => {
+      const data = res.data;
 
-  //         await this.locations.save(newLoketsi);
-  //       }
+      for (const obj of data) {
+        for (const loketsi of obj.course) {
+          console.log(loketsi);
+          const newLoketsi = this.locations.create({
+            contentid: loketsi.contentid,
+            subcontentid: loketsi.subcontentid,
+            contenttypeid: loketsi.contenttypeid,
+            name: loketsi.subname,
+            overview: loketsi.subdetailoverview,
+            img: loketsi.subdetailimg,
+          });
 
-  //       const newObj = this.courses.create({
-  //         contentid: obj.commonDetail[0].contentid,
-  //         contenttypeid: obj.commonDetail[0].contenttypeid,
-  //         areacode: obj.commonDetail[0].areacode,
-  //         title: obj.commonDetail[0].title,
-  //         overview: obj.commonDetail[0].overview,
-  //         cat1: obj.commonDetail[0].cat1,
-  //         cat2: obj.commonDetail[0].cat2,
-  //         cat3: obj.commonDetail[0].cat3,
-  //         firstimage: obj.commonDetail[0].firstimage,
-  //         firstimage2: obj.commonDetail[0].firstimage2,
-  //         mapx: obj.commonDetail[0].mapx + '',
-  //         mapy: obj.commonDetail[0].mapy + '',
-  //         mlevel: obj.commonDetail[0].mlevel,
-  //         sigungucode: obj.commonDetail[0].sigungucode,
-  //         length: obj.introduceDetail[0].distance,
-  //         taketime: obj.introduceDetail[0].taketime,
-  //       });
+          await this.locations.save(newLoketsi);
+        }
 
-  //       console.log(newObj);
-  //       await this.courses.save(newObj);
-  //     }
-  //   });
-  // }
+        const newObj = this.courses.create({
+          contentid: obj.commonDetail[0].contentid,
+          contenttypeid: obj.commonDetail[0].contenttypeid,
+          areacode: obj.commonDetail[0].areacode,
+          title: obj.commonDetail[0].title,
+          overview: obj.commonDetail[0].overview,
+          cat1: obj.commonDetail[0].cat1,
+          cat2: obj.commonDetail[0].cat2,
+          cat3: obj.commonDetail[0].cat3,
+          firstimage: obj.commonDetail[0].firstimage,
+          firstimage2: obj.commonDetail[0].firstimage2,
+          mapx: obj.commonDetail[0].mapx + '',
+          mapy: obj.commonDetail[0].mapy + '',
+          mlevel: obj.commonDetail[0].mlevel,
+          sigungucode: obj.commonDetail[0].sigungucode,
+          length: obj.introduceDetail[0].distance,
+          taketime: obj.introduceDetail[0].taketime,
+        });
+
+        console.log(newObj);
+        await this.courses.save(newObj);
+      }
+    });
+  }
 }
