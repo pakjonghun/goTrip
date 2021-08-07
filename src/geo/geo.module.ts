2b032eafee5149e4a93efb23ba6c4df57e5370fa
coffeed-cat <gwcattower@gmail.com>
2021-08-07 19:12:59 +0900

    [ geo.module.ts, geo.service.ts ] 랜덤 코스 추출 & 관광지 정보들 반환하도록 변경

--- src/geo/geo.module.ts
+++ src/geo/geo.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from 'src/trip/entities/course.entity';
+import { CourseRoute } from 'src/trip/entities/courseRoute.entity';
+import { Location } from 'src/trip/entities/location.entity';
 import { GeoService } from './geo.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, Location, CourseRoute])],
   providers: [GeoService],
   exports: [GeoService],
 })
